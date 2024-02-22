f8fdf117b5b8bc9057db37e6b534261592b2f0c3
EmiiFuentess <emiliano.fuentess@hotmail.com>
2024-02-22 13:11:47 -0600

    El cambio fue al imprimir el resultado

--- calculadora.c
+++ calculadora.c
@@ -3,35 +3,35 @@
 #include <math.h>
 
 int main() {
-    int num1, num2, resultado;
+    int n1, n2, resultado;
     char operador;
 
     printf("Pon el primer numero: ");
-    scanf("%d", &num1);
+    scanf("%d", &n1);
     printf("Que operador quieres ocupar?: ");
     scanf(" %c", &operador);
     printf("Pon el segundo numero: ");
-    scanf("%d", &num2);
+    scanf("%d", &n2);
 
     switch (operador) {
         case '+':
-            resultado = num1 + num2;
+            resultado = n1 + n2;
             break;
         case '-':
-            resultado = num1 - num2;
+            resultado = n1 - n2;
             break;
         case '*':
-            resultado = num1 * num2;
+            resultado = n1 * n2;
             break;
         case '/':
-            resultado = num1 / num2;
+            resultado = n1 / n2;
             break;
         default:
             printf("Operador inválido\n");
             return 1;
     }
 
-    printf("El resultado de la operacion: %d %c %d es: %d\n",num1,operador,num2,resultado);
+    printf("El resultado de la operacion: %d %c %d es: %d\n",n1,operador,n2,resultado);
 
     return 0;
 
