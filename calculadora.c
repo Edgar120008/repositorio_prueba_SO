07e69d774c9a9e87b8d79819b102a59e39194da7
Edgar120008 <edgarenano1208@outlook.com>
2024-02-22 07:19:47 -0600

    secambiaron las palabras del menu

--- calculadora.c
+++ calculadora.c
@@ -3,38 +3,7 @@
 #include <math.h>
 
 int main() {
-    int num1, num2, resultado;
-    char operador;
-
-    printf("| CALCULADORA |\n");
-    printf("Digite el primer número: ");
-    scanf("%d", &num1);
-    printf("Digite el operador: ");
-    scanf(" %c", &operador);
-    printf("Digite el segundo número: ");
-    scanf("%d", &num2);
-
-    switch (operador) {
-        case '+':
-            resultado = num1 + num2;
-            break;
-        case '-':
-            resultado = num1 - num2;
-            break;
-        case '*':
-            resultado = num1 * num2;
-            break;
-        case '/':
-            resultado = num1 / num2;
-            break;
-        default:
-            printf("Operador inválido...\n");
-            return 1;
-    }
-
-    printf("El Resultado es: %d\n", resultado);
-
-    return 0;
+    
     // esta es tu rama vanesa
 }
 
