32c316ef82e19c32fa440e0da0ead31fc52dfa2d
Edgar120008 <edgarenano1208@outlook.com>
2024-02-22 06:34:42 -0600

    el cambio en el archivo

---
+++ calculadora.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+    int num1, num2, resultado;
+    char operador;
+
+    printf("Digite o primeiro número: ");
+    scanf("%d", &num1);
+    printf("Digite o operador: ");
+    scanf(" %c", &operador);
+    printf("Digite o segundo número: ");
+    scanf("%d", &num2);
+
+    switch (operador) {
+        case '+':
+            resultado = num1 + num2;
+            break;
+        case '-':
+            resultado = num1 - num2;
+            break;
+        case '*':
+            resultado = num1 * num2;
+            break;
+        case '/':
+            resultado = num1 / num2;
+            break;
+        default:
+            printf("Operador inválido\n");
+            return 1;
+    }
+
+    printf("Resultado: %d\n", resultado);
+
+    return 0;
+}
